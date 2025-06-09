93b2913b9b63423e424e5c2d049ad4a0fbfcae06
Edson Moreira Bastos <moreira.bastos@outlook.com>
2025-06-09 22:26:29 +0100

    Atualizações e correções visuais no projeto SIGA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,39 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Academia Naval - Login',
+  title: 'Academia Naval',
   description: 'Página de login da Academia Naval',
+  keywords: ['academia naval', 'angola', 'marinha', 'ensino', 'universidade', 'angola'],
+  creator: 'Academia Naval',
+  publisher: 'Academia Naval',
+  applicationName: 'Academia Naval',
+  authors: [{ name: 'Academia Naval' }],
+  openGraph: {
+    title: 'Academia Naval',
+    description: 'Página de login da Academia Naval',
+    url: 'https://siga-academia-naval.vercel.app',
+    siteName: 'Academia Naval',
+    images: [
+      {
+        url: '/og-image.jpg', // Adicione uma imagem na pasta public
+        width: 1200,
+        height: 630,
+        alt: 'Academia Naval Logo',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Academia Naval - Login',
+    description: 'Página de login da Academia Naval',
+    images: ['/og-image.jpg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
